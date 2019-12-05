d652a23d6148232dd8769c78021174cdecbf8e72
Ashura <the.lucas2016@gmail.com>
2019-12-05 00:38:48 -0200

    Criar o controller e adicionar um endpoint para criar filmes.

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Uma API Rest de filmes.
 
 -   [x] Iniciar o projeto
 -   [x] Criar os models e migrations.
--   [ ] Criar o controller.
--   [ ] Adicionar endpoint para criar filmes.
+-   [x] Criar o controller.
+-   [x] Adicionar endpoint para criar filmes.
 -   [ ] Adicionar endpoint para obter filmes.
 -   [ ] Adicionar endpoint para atualizar filmes.
 -   [ ] Adicionar endpoint para deletar filmes.
---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateMovieRequest;
+use App\Http\Resources\MovieResource;
+use App\Movie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "kk";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateMovieRequest $request)
+    {
+        return new MovieResource(
+            Movie::create([
+                'title' => $request->title,
+                'year' => $request->year,
+                'thumbnail' => basename(
+                    $request->thumbnail->store('thumbnails', 'public')
+                )
+            ])
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie $movie)
+    { }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie $movie)
+    { }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $movie)
+    { }
+}
