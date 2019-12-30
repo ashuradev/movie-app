98cbf1d10cc4c356c061173057aee80500843493
Ashura <the.lucas2016@gmail.com>
2019-12-30 09:51:29 -0200

    add sagas

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './ducks';
+import rootSaga from './sagas';
 
-const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
 
@@ -10,5 +14,7 @@ const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(...middlewares))
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
